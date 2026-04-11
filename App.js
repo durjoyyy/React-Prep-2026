1a4f82572656329155d3e1e5cc7c58f222eb9b9a
Durjoy Ghosh <durjoy12091999@gmail.com>
2026-04-12 00:25:58 +0530

    parcel, babel, package json lessons

--- App.js
+++ App.js
@@ -1,26 +1,64 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+// import React from "react"
+// import ReactDOM from "react-dom/client"
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am H1"),
-    React.createElement("h2", {}, "I am H2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am H1"),
-    React.createElement("h2", {}, "I am H2"),
-  ])]
-);
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   [
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement("h1", {}, "I am H1"),
+//     React.createElement("h2", {}, "I am H2"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I am H1"),
+//     React.createElement("h2", {}, "I am H2"),
+//   ])]
+// );
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React",
+// );
+// console.log(heading);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const heading = React.createElement(
   "h1",
   { id: "heading" },
-  "Hello World from React",
+  "Durjoy's first Heading",
 );
-console.log(heading);
+const jsxHeading = (
+<h1 className="heading">Jsx Heading</h1>
+);
+
+
+const name="Durjoy";
+
+
+//short hand
+const ShortHandHeader = () => <h1 className="heading">Durjoy Short Hand</h1>
+
+//React Functional Component
+const HeadingComp = () => (
+  <div>
+    <ShortHandHeader />
+    {heading}
+    {name+ ' '+ 'Ghosh'}
+    {console.log("hello")}
+    <h1>Durjoy Ghosh's React Prep</h1>
+  </div>
+);
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComp />);
